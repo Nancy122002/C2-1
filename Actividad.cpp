4001ce6bd6101762d954de6fabed28b04d0009b8
NANCY <85953971+Nancy122002@users.noreply.github.com>
2021-07-07 09:44:26 -0500

    Update Actividad.cpp
    Se ha creado la parte principal del programa que es llenar y mostrar datos de las matrices

--- Actividad.cpp
+++ Actividad.cpp
@@ -1,1 +1,29 @@
-
+//=========================================
+//INTEGRANTES:
+//VALLECILLA NANCY 
+//JAMA PATRICIA 
+//ORTIZ EVELIN 
+//=========================================
+//========================================
+//COLABORADOR NANCY VALLECILLA
+//========================================
+#include<stdio.h>
+#include<stdlib.h>
+main (){
+	int m1[5][5],m2[5][5],m3[5][5],i,j;
+	
+	printf("primer matriz [2x3]");
+	 for(i=1;i<=2;i++)
+    	for(j=1;j<=3;j++)
+	{
+		printf(" \n introducir valor de [%d][%d]:",i,j);
+		scanf("%d",&m1[i][j]);
+	}
+	system("cls");
+	printf("segunda matriz [3x3]");
+		for(i=1;i<=3;i++)
+	for(j=1;j<=3;j++)
+    	{
+		printf("\n introducir el valor de [%d][%d]:",i,j);
+		scanf(" %d ", &m2[i][j]);
+	}
