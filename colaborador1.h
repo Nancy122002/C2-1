3516a89153171da69799dd279e43c7a79e3f1d33
NANCY <85953971+Nancy122002@users.noreply.github.com>
2021-07-11 15:51:58 -0500

    INGRESAR Y MOSTRAR LOS VALORES DE LA MATRIZ
    Ingrese y mostré valores de las 2 matrices usando la función printf  que nos permite la aparición de valores numéricos, caracteres y cadenas de texto por pantalla.
    el valor "%d” se reemplaza por el valor de la variable contador y se imprime la cadena resultante. Seguido también de un scanf que nos  permite entrar datos en la memoria del ordenador a través del teclado.

--- colaborador1.h
+++ colaborador1.h
@@ -1,9 +1,24 @@
-void ingresar(float (*m1)[5], float (*m2)[5])
-{
+#include<stdio.h>
+#include<stdlib.h>
+main (){
+	int m1[5][5],m2[5][5],m3[5][5],i,j,potencia;
+	
+	printf("primer matriz [2x3]");
+	 for(i=1;i<=2;i++)//fila
+    	for(j=1;j<=3;j++)//columas
 
-cout<<"Ingresar y mostrar los valores de los arreglos o matrices";
+	{
+		printf(" \n introducir valor de [%d][%d]:",i,j);
+		scanf(" %d ",&m1[i][j]);
+		
+	}
+	system("cls");
+	printf("\n");
+	printf("segunda matriz [3x3]");
+		for(i=1;i<=3;i++)//fila
+        	for(j=1;j<=3;j++)//columas
+	{
+		printf("\n introducir el valor de [%d][%d]:",i,j);
+		scanf(" %d ",&m2[i][j]);
+	}
 
-
-
-
-}
