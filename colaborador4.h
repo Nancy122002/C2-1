94a837aa54cfe515339fa5d58f98d5b174466549
Patricia Jama <86250193+patriciajama@users.noreply.github.com>
2021-07-12 17:36:32 -0500

    Update colaborador4.h
    Empieza a leer los datos del usuario, con dos for uno cumple la función de ir llenando la fila y el otro de saltar al siguiente fila para que sea llenada después de que se cumple el ciclo de llenado, ahora se pasa a la rutina para imprimir los valores de forma de una matriz, para que el usuario pueda comparar la matriz original a la matriz transpuesta, después de la rutina de imprimir la matriz original, imprimir la matriz transpuesta es muy sencillo solo se intercambia en el for el orden en que se impriman los datos, en vez de imprimir matriz[i][j], se imprime matriz[j][i].

--- colaborador4.h
+++ colaborador4.h
@@ -4,6 +4,24 @@ void transpuesta(float (*m1)[5], float (*r)[5])
 cout<<"Obtener la transpuesta de una matriz";
 
 
-
+}
+	printf ("\n MATRIZ ORIGINAL \n  ");
+	printf(" \n ");
+	for(i=1;i<=2;i++);{
+		for(j=1;j<=3;j++){
+		printf(" %d ", m1[i][j]);
+			printf(" \n ");
+	}
+	}
+	printf("\n MATRIZ TRANSMPUESTA \n ");
+	for( i=1;i<=2;i++){
+		for( j=1;j<=3;j++){
+			printf (" %d ", m1[j][i]);
+			printf(" \n ");
+		}
+	}
+	system("pause");
+		
+}
 
 }
