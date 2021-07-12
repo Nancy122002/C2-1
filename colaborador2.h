d5f35eb0192a2bfa02331cf25ae4105c2c4fb4c5
evelinortiz <86195971+evelinortiz@users.noreply.github.com>
2021-07-12 08:40:55 -0500

    Update colaborador2.h
    Se genera el producto entre dos matrices, para multiplicar dos matrices el orden de las matrices importa ya que el número de columnas de la primera matriz debe ser igual al número de filas de la segunda. La matriz resultante tendrá como orden el número de filas de la primera matriz por el número de columnas de la segunda matriz. Así, por ejemplo, si A [2] [3] y B [1] [3] entonces la matriz resultante C deberá ser de C [2] [3]

--- colaborador2.h
+++ colaborador2.h
@@ -7,3 +7,17 @@ cout<<"Obetener el producto de las dos matrices ";
 
 
 }
+system("cls");
+printf("\n PRODUCTO DE MATRIZ\n");
+for(i=1;i<=2;i++)//fila
+for(j=1;j<=3;j++)//columas
+m3[i][j]=(m1[i][1]*m2[1][j])+(m1[i][2]*m2[2][j])+(m1[i][3]*m2[3][j]);
+
+for(i=1;i<=2;i++)//fila
+{
+for(j=1;j<=3;j++)//columnas
+    printf(" %d ",m3[i][j]);
+printf(" \n ");
+ 
+
+}
